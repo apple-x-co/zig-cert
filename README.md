bc07cbe4b4b95d4c096316ce42bbfe5b0bd0d98b
Kouhei.Sano <apple.x.co@gmail.com>
2023-09-23 18:15:15 +0900

    chore: zig 0.11.0 build

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # zig-cert
+
+## Build
+
+```bash
+zig build -Doptimize=ReleaseFast
+```
+
+## Run
+
+```bash
+zig build run -- example.com
+```
--- src/main.zig
+++ src/main.zig
@@ -1,60 +1,22 @@
 const std = @import("std");
-const tcp = std.x.net.tcp;
 
-const openssl = @cImport({
-    @cInclude("openssl/ssl.h");
-    @cInclude("openssl/err.h");
-});
-
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-
-    // const client = try tcp.Client.init(.ip, .{ .close_on_exec = true });
-    // defer client.deinit();
-
-    // client.connect(std.x.net.ip.Address.from(.{"googl.com"}));
-
-    // std.x.net.tcp.Domain.ip
-
+pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
 
-    // https://github.com/search?l=zig&q=std.net.tcpConnectToHost&type=Code
-    // https://github.com/genkiroid/cert/blob/master/cert.go
-    const tcp_conn = try std.net.tcpConnectToHost(allocator, "google.com", 443);
-    defer tcp_conn.close();
+    const host = "example.com";
+    const port = 443;
+    const stream = try std.net.tcpConnectToHost(allocator, host, port);
+    defer stream.close();
 
-    //////
-    // var buf: []u8 = undefined;
-    // var size = try tcp_conn.read(buf);
+    var ca_bundle = std.crypto.Certificate.Bundle{};
+    defer ca_bundle.deinit(allocator);
+    try ca_bundle.rescan(allocator);
 
-    // std.log.debug("{s}", .{tcp_conn});
-    // std.log.debug("{s}", .{buf});
-    // std.log.debug("{d}", .{size});
+    var client = try std.crypto.tls.Client.init(stream, ca_bundle, host);
+    _ = client;
 
-    //////
-    // https://ken-ohwada.hatenadiary.org/entry/2021/02/27/113436
-    // https://github.com/haze/zig-libressl/tree/main
-    // https://qiita.com/edo_m18/items/41770cba5c166f276a83
-    //
-    // https://github.com/search?l=zig&q=OPENSSL_init_ssl&type=Code
-    // https://github.com/marler8997/ziget/blob/5b6f0667c906e1c76b71317a599d9a3831d50661/openssl/ssl.zig
-    // https://github.com/fubark/cosmic/blob/0bfed2520d1f67203014a9c3330801c745f34cca/lib/openssl/openssl.zig
-
-    std.debug.print("[DEBUG] openssl init\n", .{});
-    //std.log.debug("{}", .{openssl});
-
-    if (openssl.OPENSSL_init_ssl(0, null) != 1) {
-        return error.InitializationError;
-    }
-
-    // FIXME: 以下のエラーをどうにかする
-    // error(link): undefined reference to symbol '_OPENSSL_init_ssl'
-    // error(link):   first referenced in '/path/to/zig-cert/zig-cache/o/b31ad817d877ac41188e138e31aeb0b9/zig-cert.o'
-    // error: UndefinedSymbolReference
-}
-
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+    // FIXME: 証明書の情報を取得したい
+    // const parsed = try std.crypto.Certificate.parse(.{ .buffer = ca_bundle.bytes.items, .index = 0 });
+    // std.debug.print("{s}", .{parsed.subject()});
 }
